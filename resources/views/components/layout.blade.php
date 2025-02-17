2b21ebf8bbe44616abaf1c81e5a11e6286c8fc6d
achraf <achrafsikal@gmail.com>
2025-02-17 11:27:28 +0100

    configuring tailwindcss and vite-plugin

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>home</title>
+    @vite('resources/css/app.css')
 </head>
+
 <body>
 <nav>
         <x-nav-link href="/">home</x-nav-link>
